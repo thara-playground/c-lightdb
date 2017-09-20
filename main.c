8b11d39983d93721342ce7e996b1ea0ca4cdeaf7
Tomochika Hara <tomochikahara@gmail.com>
2017-09-21 00:52:23 +0900

    Add a simple REPL

--- main.c
+++ main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} input_buffer_t;
+
+input_buffer_t* new_input_buffer() {
+    input_buffer_t* bf = malloc(sizeof(input_buffer_t));
+    bf->buffer = NULL;
+    bf->buffer_length = 0;
+    bf->input_length = 0;
+    return bf;
+}
+
+void print_prompt() { printf("db > "); }
+
+void read_input(input_buffer_t* input) {
+    ssize_t bytes_read = getline(&(input->buffer), &(input->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input->input_length = bytes_read - 1;   // Ignore trailing newline
+    input->buffer[bytes_read - 1] = 0;
+}
+
+int main(int argc, char* argv[]) {
+    input_buffer_t* input = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input);
+
+        if (strcmp(input->buffer, ".exit") == 0) {
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'.\n", input->buffer);
+        }
+    }
+
     return 0;
 }
